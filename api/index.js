97a9d2fabe279b54f04cdcd5e1a84b1243074912
Nishant Mittal <mittalnishant14@outlook.com>
2022-01-26 03:39:31 +0530

    fix: bug in function

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 module.exports = async (req, res) => {
-    const { data } = axios.get("https://raw.githubusercontent.com/joplin/plugins/master/stats.json");
+    const { data } = await axios.get("https://raw.githubusercontent.com/joplin/plugins/master/stats.json");
     const allPluginReleases = data["joplin.plugin.templates"];
     let totalDownloads = 0;
     for (const o of Object.values(allPluginReleases)) {
