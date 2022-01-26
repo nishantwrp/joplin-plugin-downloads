1e58e892ae52b6b48d9669f9b4da87140701883c
Nishant Mittal <mittalnishant14@outlook.com>
2022-01-26 22:40:34 +0530

    feat: generalize function for all plugins

--- api/index.js
+++ api/index.js
@@ -1,11 +1,40 @@
 const axios = require("axios").default;
 
-module.exports = async (req, res) => {
-    const { data } = await axios.get("https://raw.githubusercontent.com/joplin/plugins/master/stats.json");
-    const allPluginReleases = data["joplin.plugin.templates"];
+const getPluginDownloads = (allReleases) => {
     let totalDownloads = 0;
-    for (const o of Object.values(allPluginReleases)) {
-        totalDownloads += o.downloadCount;
+    let latestRelease = null;
+
+    Object.entries(allReleases).forEach(([release, details]) => {
+        totalDownloads += details.downloadCount;
+        latestRelease = {
+            version: release,
+            ...details,
+        };
+    });
+
+    return {
+        totalDownloads,
+        latestRelease,
+        allReleases,
+    };
+}
+
+module.exports = async (req, res) => {
+    const pluginId = req.query?.plugin;
+    const { data } = await axios.get("https://raw.githubusercontent.com/joplin/plugins/master/stats.json");
+
+    if (pluginId) {
+        if (Object.keys(data).includes(pluginId)) {
+            res.json(getPluginDownloads(data[pluginId]));
+        } else {
+            res.status(404);
+            res.json({ error: 'No plugin with this id found!' });
+        }
+    } else {
+        const allPlugins = {};
+        Object.entries(data).forEach(([plugin, releaseData]) => {
+            allPlugins[plugin] = getPluginDownloads(releaseData);
+        });
+        res.json(allPlugins);
     }
-    res.json({ downloads: totalDownloads });
 }
